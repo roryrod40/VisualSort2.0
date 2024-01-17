df1e0c37632a63d72ab2346e07042cf6915e41a8
roryrod40 <rodriguez.rory40@gmail.com>
2024-01-16 23:36:52 -0500

    Added mongoose and morgan

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const { MongoClient } = require('mongodb');
 
